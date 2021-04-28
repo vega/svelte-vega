40de7aad481fcc6dc52e79b377d22451cdbfb7ce
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2021-04-28 11:41:32 +0200

    fix: updated vega lite demo to vega lite spec
    The VegaLite demo now really uses the VegaLite spec.

--- packages/storybook/stories/SvelteVegaLiteDemo.svelte
+++ packages/storybook/stories/SvelteVegaLiteDemo.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
-  import type { EmbedOptions } from "vega-embed";
+  import type { EmbedOptions, VisualizationSpec } from "vega-embed";
   import VegaLite from "../../svelte-vega/src/VegaLite.svelte";
   import data1 from "./data1.json";
-  import spec1 from "./spec1";
-  import spec2 from "./spec2";
 
   export let options: EmbedOptions = {};
 
-  let info = "";
   let data = data1;
-  let spec = spec1;
-  const handlers: {
-    tooltip: (...args: unknown[]) => void;
-  } = { tooltip: (...args) => (info = JSON.stringify(args)) };
-
-  function handleToggleSpec() {
-    if (spec === spec1) {
-      spec = spec2;
-    } else {
-      spec = spec1;
-    }
-  }
+  let spec: VisualizationSpec = {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    description: "A simple bar chart with embedded data.",
+    data: {
+      name: "table",
+    },
+    mark: "bar",
+    encoding: {
+      x: { field: "category", type: "nominal" },
+      y: { field: "amount", type: "quantitative" },
+    },
+  };
 
   function handleUpdateData() {
     const table = [];
@@ -45,17 +42,13 @@
       height="20px"
     />
   </div>
-  <button on:click={handleToggleSpec}>Toggle Spec</button>
   <button on:click={handleUpdateData}>Update Data</button>
   <div class="content">
     <h3>
       <code>&lt;VegaLite&gt;</code> Svelte Component
     </h3>
     Will recompile when spec changes and update when data changes.
-    <VegaLite {data} {spec} {options} signalListeners={handlers} />
-    <div>
-      Hover info: <code>{info}</code>
-    </div>
+    <VegaLite {data} {spec} {options} />
   </div>
 </main>
 
