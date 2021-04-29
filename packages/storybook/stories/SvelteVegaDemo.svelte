75263fa29698ab3e373a0ce797c6b3c619cdbddb
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2021-04-29 12:45:29 +0200

    feat: link to github in storybook
    Storybook stories now have a link to the project.
    
    fix #15

--- packages/storybook/stories/SvelteVegaDemo.svelte
+++ packages/storybook/stories/SvelteVegaDemo.svelte
@@ -4,6 +4,7 @@
   import data1 from "./data1.json";
   import spec1 from "./spec1";
   import spec2 from "./spec2";
+  import githubImage from "./GitHub.png";
 
   export let options: EmbedOptions = {};
 
@@ -34,28 +35,31 @@
   }
 </script>
 
-<main>
-  <div style={"float: right"}>
-    <iframe
-      title="star"
-      src="https://ghbtns.com/github-btn.html?user=vega&repo=svelte-vega&type=star&count=true"
-      frameBorder="0"
-      scrolling="0"
-      width="100px"
-      height="20px"
-    />
-  </div>
-  <button on:click={handleToggleSpec}>Toggle Spec</button>
-  <button on:click={handleUpdateData}>Update Data</button>
-  <div class="content">
-    <h3>
-      <code>&lt;Vega&gt;</code> Svelte Component
-    </h3>
-    Will recompile when spec changes and update when data changes.
-    <Vega {data} {spec} {options} signalListeners={handlers} />
-    <div>
-      Hover info: <code>{info}</code>
+<main class="content">
+  <div class="head">
+    <div class="row-content">
+      <h3>
+        <code>&lt;Vega&gt;</code> Svelte Component
+      </h3>
+      <button on:click={handleToggleSpec} class="common-button"
+        >Toggle Spec</button
+      >
+      <button on:click={handleUpdateData} class="common-button"
+        >Update Data</button
+      >
     </div>
+    <button
+      class="github-button"
+      on:click={() => window.open("https://github.com/vega/svelte-vega")}
+    >
+      <img src={githubImage} alt="github mark" />
+      <div>View on GitHub</div>
+    </button>
+  </div>
+  Will recompile when spec changes and update when data changes.
+  <Vega {data} {spec} {options} signalListeners={handlers} />
+  <div>
+    Hover info: <code>{info}</code>
   </div>
 </main>
 
@@ -63,5 +67,67 @@
   .content {
     display: flex;
     flex-direction: column;
+    font-family: "SF Pro Text", "Myriad Set Pro", "SF Pro Icons",
+      "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+
+  .row-content {
+    display: flex;
+    align-items: center;
+  }
+
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .github-button {
+    display: flex;
+    align-items: center;
+    height: 2em;
+    margin: 1em;
+    padding: 0.5em;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
+  .github-button:active {
+    background-color: #eee;
+  }
+
+  .common-button {
+    display: flex;
+    align-items: center;
+    height: 2em;
+    margin-left: 0.5em;
+    padding: 0.5em;
+    border: 1px solid #5382b0;
+    border-radius: 5px;
+  }
+
+  .common-button:active {
+    background-color: #cce6ff;
+  }
+
+  img {
+    height: 100%;
+    padding-right: 0.5em;
+  }
+
+  button {
+    font-family: inherit; /* 1 */
+    font-size: 100%; /* 1 */
+    line-height: 1.15; /* 1 */
+    margin: 0; /* 2 */
+    background-color: transparent;
+    text-transform: none;
+    border: none;
+    overflow: visible;
+    -webkit-appearance: button;
+  }
+
+  button:active {
+    color: inherit;
   }
 </style>
--- packages/storybook/stories/SvelteVegaLiteDemo.svelte
+++ packages/storybook/stories/SvelteVegaLiteDemo.svelte
@@ -2,6 +2,7 @@
   import type { EmbedOptions, VisualizationSpec } from "vega-embed";
   import VegaLite from "../../svelte-vega/src/VegaLite.svelte";
   import data1 from "./data1.json";
+  import githubImage from "./GitHub.png";
 
   export let options: EmbedOptions = {};
 
@@ -31,30 +32,93 @@
   }
 </script>
 
-<main>
-  <div style={"float: right"}>
-    <iframe
-      title="star"
-      src="https://ghbtns.com/github-btn.html?user=vega&repo=svelte-vega&type=star&count=true"
-      frameBorder="0"
-      scrolling="0"
-      width="100px"
-      height="20px"
-    />
+<main class="content">
+  <div class="head">
+    <div class="row-content">
+      <h3>
+        <code>&lt;VegaLite&gt;</code> Svelte Component
+      </h3>
+      <button on:click={handleUpdateData} class="common-button"
+        >Update Data</button
+      >
+    </div>
+    <button
+      class="github-button"
+      on:click={() => window.open("https://github.com/vega/svelte-vega")}
+    >
+      <img src={githubImage} alt="github mark" />
+      <div>View on GitHub</div>
+    </button>
   </div>
-  <button on:click={handleUpdateData}>Update Data</button>
-  <div class="content">
-    <h3>
-      <code>&lt;VegaLite&gt;</code> Svelte Component
-    </h3>
-    Will recompile when spec changes and update when data changes.
-    <VegaLite {data} {spec} {options} />
-  </div>
+  Will recompile when spec changes and update when data changes.
+  <VegaLite {data} {spec} {options} />
 </main>
 
 <style>
   .content {
     display: flex;
     flex-direction: column;
+    font-family: "SF Pro Text", "Myriad Set Pro", "SF Pro Icons",
+      "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+
+  .row-content {
+    display: flex;
+    align-items: center;
+  }
+
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .github-button {
+    display: flex;
+    align-items: center;
+    height: 2em;
+    margin: 1em;
+    padding: 0.5em;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
+  .github-button:active {
+    background-color: #eee;
+  }
+
+  .common-button {
+    display: flex;
+    align-items: center;
+    height: 2em;
+    margin-left: 0.5em;
+    padding: 0.5em;
+    border: 1px solid #5382b0;
+    border-radius: 5px;
+  }
+
+  .common-button:active {
+    background-color: #cce6ff;
+  }
+
+  img {
+    height: 100%;
+    padding-right: 0.5em;
+  }
+
+  button {
+    font-family: inherit; /* 1 */
+    font-size: 100%; /* 1 */
+    line-height: 1.15; /* 1 */
+    margin: 0; /* 2 */
+    background-color: transparent;
+    text-transform: none;
+    border: none;
+    overflow: visible;
+    -webkit-appearance: button;
+  }
+
+  button:active {
+    color: inherit;
   }
 </style>
