133f0bc841222497b429a1e6ae9efbf983fc3602
Alex Cabrera <alex.cabrera@gmail.com>
2021-05-03 11:10:36 -0600

    fix: update to non-deprecated json plugin in sample

--- packages/sample-project/rollup.config.js
+++ packages/sample-project/rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import * as child from "child_process";
 
 const production = !process.env.ROLLUP_WATCH;
