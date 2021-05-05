a1d8be9563d70d0b64ee6abe2dd4830fa30ffc1c
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2021-05-05 14:46:24 +0200

    chore: added Vega and VegaLite specs
    Now also individually exports Vega and VegaLite specs.
    
    re #29

--- packages/svelte-vega/src/types.ts
+++ packages/svelte-vega/src/types.ts
@@ -1,4 +1,6 @@
 import type { Result, VisualizationSpec as Spec } from "vega-embed";
+import type { Spec as VgSpec } from "vega";
+import type { TopLevelSpec as VlSpec } from "vega-lite";
 
 export type View = Result["view"];
 
@@ -9,3 +11,5 @@ export type SignalListeners = {
 };
 
 export type VisualizationSpec = Spec;
+export type VegaSpec = VgSpec;
+export type VegaLiteSpec = VlSpec;
