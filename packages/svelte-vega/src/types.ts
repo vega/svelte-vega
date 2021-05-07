306d5f34605c88e2facce4c75dbcf8e979cdbc10
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2021-05-07 09:28:59 +0200

    feat: svelte-vega now exports VisualizationSpec (#30)
    * chore: svelte-vega now exports VisualizationSpec
    
    Now exporting VisualizationSpec directly from svelte-vega, so no need to import vega-embed for that
    anymore.
    
    re #29
    
    * chore: added Vega and VegaLite specs
    
    Now also individually exports Vega and VegaLite specs.
    
    re #29

--- packages/svelte-vega/src/types.ts
+++ packages/svelte-vega/src/types.ts
@@ -1,4 +1,6 @@
-import type { Result } from "vega-embed";
+import type { Result, VisualizationSpec as Spec } from "vega-embed";
+import type { Spec as VgSpec } from "vega";
+import type { TopLevelSpec as VlSpec } from "vega-lite";
 
 export type View = Result["view"];
 
@@ -7,3 +9,7 @@ export type SignalListener = (name: string, value: unknown) => void;
 export type SignalListeners = {
   [key: string]: SignalListener;
 };
+
+export type VisualizationSpec = Spec;
+export type VegaSpec = VgSpec;
+export type VegaLiteSpec = VlSpec;
