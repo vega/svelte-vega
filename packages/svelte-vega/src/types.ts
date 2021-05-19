a9a901352d9994195409024d748bdc1fdc5f50bf
Dominik Moritz <domoritz@gmail.com>
2021-05-19 16:34:10 -0700

    refactor: clean up view export

--- packages/svelte-vega/src/types.ts
+++ packages/svelte-vega/src/types.ts
@@ -1,6 +1,5 @@
-export { View } from "vega";
+export type { Spec as VegaSpec, View } from "vega";
 export type { VisualizationSpec } from "vega-embed";
-export type { Spec as VegaSpec } from "vega";
 export type { TopLevelSpec as VegaLiteSpec } from "vega-lite";
 
 export type SignalListener = (name: string, value: unknown) => void;
