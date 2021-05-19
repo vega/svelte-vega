cd9d5b7af0ab3a760ecd89814c4d77179ac214a8
Dominik Moritz <domoritz@gmail.com>
2021-05-19 15:41:54 -0700

    feat: expose VegaSpec and VegaLiteSpec

--- packages/svelte-vega/src/types.ts
+++ packages/svelte-vega/src/types.ts
@@ -1,5 +1,7 @@
 export { View } from 'vega';
 export type { VisualizationSpec } from 'vega-embed';
+export type { Spec as VegaSpec } from "vega";
+export type { TopLevelSpec as VegaLiteSpec } from "vega-lite";
 
 export type SignalListener = (name: string, value: unknown) => void;
 
