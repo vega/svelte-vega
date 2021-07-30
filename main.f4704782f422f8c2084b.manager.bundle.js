55bc40786429c0337a4875badd0340c8e42ad06c
Sparkier <alex.baeuerle@uni-ulm.de>
2021-07-30 12:13:11 +0000

    Deploying to gh-pages from @ vega/svelte-vega@e18b2c36ca98a6f3e57cefdadf19609fed47c217 🚀

---
+++ main.f4704782f422f8c2084b.manager.bundle.js
@@ -0,0 +1,1 @@
+(globalThis.webpackChunksvelte_vega_demo=globalThis.webpackChunksvelte_vega_demo||[]).push([[179],{53260:()=>{}},__webpack_require__=>{var __webpack_exec__=moduleId=>__webpack_require__(__webpack_require__.s=moduleId);__webpack_require__.O(0,[389],(()=>(__webpack_exec__(1695),__webpack_exec__(18450))));__webpack_require__.O()}]);
