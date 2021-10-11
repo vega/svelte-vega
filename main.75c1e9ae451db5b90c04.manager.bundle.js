c18e24b3338b9ed6440a9da76a6e984d4b9b0782
domoritz <domoritz@gmail.com>
2021-10-11 13:14:56 +0000

    Deploying to gh-pages from @ vega/svelte-vega@0176386faa43864ee3dd9c40e6e1ce84c4c2da05 🚀

--- main.75c1e9ae451db5b90c04.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(globalThis.webpackChunksvelte_vega_demo=globalThis.webpackChunksvelte_vega_demo||[]).push([[179],{53260:()=>{}},__webpack_require__=>{var __webpack_exec__=moduleId=>__webpack_require__(__webpack_require__.s=moduleId);__webpack_require__.O(0,[480],(()=>(__webpack_exec__(1695),__webpack_exec__(46617))));__webpack_require__.O()}]);
