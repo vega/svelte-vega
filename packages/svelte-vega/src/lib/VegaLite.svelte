6621b8ac4f06d7d11749f47cb6a4ff76ddcbfd37
Alex Bäuerle <alex@a13x.io>
2023-04-13 05:44:04 -0700

    feat: change package setup to use vite (#829)
    BREAKING CHANGE: uses vite and svelte kit

---
+++ packages/svelte-vega/src/lib/VegaLite.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { EmbedOptions, Mode } from 'vega-embed';
+	import type { SignalListeners, VisualizationSpec, View } from './types';
+	import VegaEmbed from './VegaEmbed.svelte';
+
+	export let spec: VisualizationSpec;
+	export let options: EmbedOptions = {};
+	export let data: Record<string, unknown> = {};
+	export let signalListeners: SignalListeners = {};
+	export let view: View | undefined = undefined;
+
+	const mode = 'vega-lite' as Mode;
+	$: vegaLiteOptions = { ...options, mode: mode };
+</script>
+
+<VegaEmbed
+	{spec}
+	{data}
+	{signalListeners}
+	options={vegaLiteOptions}
+	bind:view
+	on:onNewView
+	on:onError
+/>
