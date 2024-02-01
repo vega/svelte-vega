d9198a96a5d7a935413c310ad19096d6b9acac1d
Dominik Moritz <domoritz@gmail.com>
2024-02-01 17:01:45 -0500

    chore: add commands for svelte and sveltekit examples (#910)

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,7 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
 module.exports = {
 	stories: ['../stories/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-mdx-gfm'],
 	framework: {
 		name: '@storybook/svelte-vite',
 		options: {
