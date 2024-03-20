e60aa94c4aa90e568a1a48c2ae56f456bf08c602
Dominik Moritz <domoritz@gmail.com>
2024-03-20 13:53:04 -0400

    feat: export embed options
    fixes #918

--- packages/svelte-vega/src/lib/types.ts
+++ packages/svelte-vega/src/lib/types.ts
@@ -1,5 +1,5 @@
 export type { Spec as VegaSpec, View } from 'vega';
-export type { VisualizationSpec } from 'vega-embed';
+export type { VisualizationSpec, EmbedOptions } from 'vega-embed';
 export type { TopLevelSpec as VegaLiteSpec } from 'vega-lite';
 
 export type SignalListener = (name: string, value: unknown) => void;
